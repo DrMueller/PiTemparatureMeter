ed9a78aa9be2b99ab596f1fc1ce059b2f7c4e145
Matthias Müller <matthias.mueller@trivadis.com>
2019-09-17 13:24:47 +0200

    WIP

---
+++ Sources/Application/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+using Mmu.Mlh.RaspberryPi.Areas.SenseHats.Models;
+using Mmu.Mlh.RaspberryPi.Areas.SenseHats.Services;
+using Mmu.Mlh.ServiceProvisioning.Areas.Initialization.Models;
+using Mmu.Mlh.ServiceProvisioning.Areas.Initialization.Services;
+using Mmu.Mlh.ServiceProvisioning.Areas.Provisioning.Services;
+
+namespace Mmu.Ptm
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var containerConfig = ContainerConfiguration.CreateFromAssembly(typeof(Program).Assembly);
+            var serviceLocator = ContainerInitializationService
+                .CreateInitializedContainer(containerConfig)
+                .GetInstance<IServiceLocator>();
+
+            var senseHatFactory = serviceLocator.GetService<ISenseHatFactory>();
+            var senseHat = senseHatFactory.Create();
+
+            using (var timer = new Timer(1000))
+            {
+                timer.Elapsed += (object sender, ElapsedEventArgs e) => ReadAndShowTemperature(senseHat);
+                timer.Start();
+                Console.ReadKey();
+            }
+        }
+
+        private static void ReadAndShowTemperature(SenseHat senseHat)
+        {
+            var task = Task.Run(async () =>
+            {
+                var temparature = await senseHat.TemparatureSensor.ReadTemparature();
+                await senseHat.LedMatrix.ShowMessage(temparature.AsDescription());
+            });
+
+            Task.WaitAll(task);
+        }
+    }
+}
